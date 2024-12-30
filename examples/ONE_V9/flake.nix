3e5d7ab566add5c38a193407c76b5e262e93064f
Michael Lynch <git@mtlynch.io>
2024-12-29 19:56:57 -0500

    Update naming in Nix flake

--- examples/ONE_V9/flake.nix
+++ examples/ONE_V9/flake.nix
@@ -5,23 +5,26 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     # 0.35.1 release
-    arduino_cli_dep.url = "github:NixOS/nixpkgs/5f5210aa20e343b7e35f40c033000db0ef80d7b9";
+    arduino-cli-nixpkgs.url = "github:NixOS/nixpkgs/5f5210aa20e343b7e35f40c033000db0ef80d7b9";
 
     # 3.12.0 release
-    python_dep.url = "github:NixOS/nixpkgs/e2b8feae8470705c3f331901ae057da3095cea10";
+    python-nixpkgs.url = "github:NixOS/nixpkgs/e2b8feae8470705c3f331901ae057da3095cea10";
   };
 
-  outputs = { self, flake-utils, arduino_cli_dep, python_dep }@inputs :
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      arduino_cli_dep = inputs.arduino_cli_dep.legacyPackages.${system};
-      python_dep = inputs.python_dep.legacyPackages.${system};
-    in
-    {
-      devShells.default = arduino_cli_dep.mkShell {
+  outputs = {
+    self,
+    flake-utils,
+    arduino-cli-nixpkgs,
+    python-nixpkgs,
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      arduino-cli = arduino-cli-nixpkgs.legacyPackages.${system}.arduino-cli;
+      python = python-nixpkgs.legacyPackages.${system}.python312;
+    in {
+      devShells.default = arduino-cli-nixpkgs.legacyPackages.${system}.mkShell {
         packages = [
-          arduino_cli_dep.arduino-cli
-          python_dep.python312
+          arduino-cli
+          python
         ];
 
         shellHook = ''
